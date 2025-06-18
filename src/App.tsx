0e864137dc417fd0509c5d2ebd39d021cb439b71
cassmatheuss <cassmatheuss@gmail.com>
2025-06-18 00:40:57 -0300

    initial commit dev

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
     <div className="flex items-center justify-center min-h-screen">
       <h1 className="text-3xl text-white">
-        Cifras do Amor
+        Cifras do Amor ❤️
       </h1>
     </div>
     </>
