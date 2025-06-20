9f6c44adb1d914857f89e644e4b6b115233bdd1f
cassmatheuss <cassmatheuss@gmail.com>
2025-06-20 18:32:40 -0300

    fix: remove import

--- src/screens/Admin.tsx
+++ src/screens/Admin.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import AddButton from '../assets/components/telaAdmin/AddButton';
 import EditableButton from '../assets/components/telaAdmin/EditableButton';
